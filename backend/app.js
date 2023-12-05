ae3da94bd7eeccf3c9ef0f86e839029eee6ecdd2
LuLesDLC <luis.ct.luis@gmail.com>
2023-12-05 09:26:57 -0500

    Rutas y funciones

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import posturasRutas from "./posturas_rutas.js";
+import info_ruta from "./info_ruta.js";
+
+const app = express();
+
+app.use(express.json());
+app.use("/", posturasRutas);
+app.use("/",info_ruta)
+
+app.listen(3000, () => {
+  console.log("Servidor escuchando en http://localhost:3000");
+});
+
