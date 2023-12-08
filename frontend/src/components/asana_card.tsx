3851ea667220d7938c5dbec81dbd6eaad3df43c3
Chrisathan12 <chris.athan12@gmail.com>
2023-12-07 19:37:36 -0500

    Ajuste pantalla <asana_card> y agrege de morphemas

--- frontend/src/components/asana_card.tsx
+++ frontend/src/components/asana_card.tsx
@@ -6,17 +6,17 @@ type AsanaCardProps = {
     name_en: string;
     image: string;
     link: string;
+    morpheme: string;
 }
 
-export function AsanaCard({name_es, sanskrit, name_en, image, link} : AsanaCardProps) {
+export function AsanaCard({name_es, sanskrit, name_en, image, link, morpheme} : AsanaCardProps) {
     return (
         <div id='card-container'>
-            <a href={link}>
-                <img src={image} alt={name_en + ' posture'} />
-                <h2>{name_es}</h2>
-                <h3>{sanskrit}</h3>
-                <h3>{name_en}</h3>
-            </a>
+            <img src={image} alt={name_en + ' posture'} />
+            <h2>{name_es}</h2>
+            <h3>{sanskrit}</h3>
+            <h3>{name_en}</h3>
+            <h1>{morpheme}</h1>
         </div>
     )
 }
